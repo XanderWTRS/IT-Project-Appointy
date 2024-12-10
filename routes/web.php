d0174cbf2c094c6fb81c754e148d749f6f8b2933
matmax55 <matteo.sprimont@student.ehb.be>
2024-12-10 13:44:35 +0100

    klanten page vieuw

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/klanten', function () {
+    return Inertia::render('KlantenPage'); 
+})->name('klanten');
+
 require __DIR__.'/auth.php';
