792700073fce1b2a621f58958aa762a8f6423ce7
lm00ns <lucasmoons@hotmail.com>
2024-12-12 14:08:39 +0100

    Othodontie page

--- resources/js/Pages/EndodontiePage.jsx
+++ resources/js/Pages/EndodontiePage.jsx
@@ -51,8 +51,6 @@ export default function EndodontiePage() {
                             </p>
                         </div>
                 </div>
-
-
             </main>
 
             <Footer />
