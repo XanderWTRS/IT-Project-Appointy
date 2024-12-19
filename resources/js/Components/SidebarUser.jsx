f98a7bfa69812a1d32db9acae0f0d5b8cb37a2ad
Xander Wauters <xwauters@outlook.com>
2024-12-19 15:45:24 +0100

    - Added the sidebar to the user profilepages. - Added module extensions - styling

--- resources/js/Components/SidebarUser.jsx
+++ resources/js/Components/SidebarUser.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import { Link } from '@inertiajs/react'; // If you're using Inertia's Link component
-import '/resources/css/SidebarUser.css';
 
-const Sidebar = () => {
+const SidebarUser = () => {
     console.log("Sidebar is being rendered");
     return (
-        <div className="sidebar">
-            <ul className="sidebar-links">
-                {/* Profiel -> uses the named route: profile.edit */}
+        <div className="w-1/4 bg-white p-4 rounded-md shadow-md h-full">
+            <ul className="space-y-4">
                 <li>
-                    <Link href={route('profile.edit')}>
-                        Profiel
-                    </Link>
+                    <a href="/profile/edit" className="relative px-4 py-2 text-gray-700 font-semibold rounded flex items-center">
+                        <img src="/Assets/Icons/Profile.svg" alt="icon of the profile" className="w-8 h-8 rounded-full inline-block mr-4" />
+                        <span className="relative after:content-[''] after:absolute after:left-1/2 after:bottom-[-4px] after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full">
+                            Profiel
+                        </span>
+                    </a>
                 </li>
-
-                {/* Afspraken -> uses the named route: afspraken */}
                 <li>
-                    <Link href={route('afspraken')}>
-                        Afspraken
-                    </Link>
+                    <a href="/afspraken" className="relative px-4 py-2 text-gray-700 font-semibold rounded flex items-center">
+                        <img src="/Assets/Icons/Calendar.svg" alt="icon of the profile" className="w-8 h-8 inline-block mr-4" />
+                        <span className="relative after:content-[''] after:absolute after:left-1/2 after:bottom-[-4px] after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:origin-center hover:after:left-0 hover:after:w-full">
+                            Afspraken
+                        </span>
+                    </a>
                 </li>
-
-                {/* Meldingen -> No named route given in web.php. If you have one, use it here.
-                    Otherwise, link to a placeholder or remove this link. */}
                 <li>
-                    <Link href={route('meldingen')}>
-                        Meldingen
-                    </Link>
+                    <a href="/meldingen" className="relative px-4 py-2 text-gray-700 font-semibold rounded flex items-center group">
+                        <img src="/Assets/Icons/Bell.svg" alt="icon of the bell" className="w-8 h-8 rounded-full inline-block mr-4 transition-transform duration-500 group-hover:animate-swing" />
+                        <span className="relative after:content-[''] after:absolute after:left-1/2 after:bottom-[-4px] after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-300 after:origin-center group-hover:after:left-0 group-hover:after:w-full">
+                            Meldingen
+                        </span>
+                    </a>
                 </li>
             </ul>
         </div>
     );
 };
 
-export default Sidebar;
+export default SidebarUser;
+
