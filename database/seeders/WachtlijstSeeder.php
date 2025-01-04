fc79134dc8a72ced63bd653727605b6895dd1f1e
Xander Wauters <xwauters@outlook.com>
2025-01-04 23:19:38 +0100

    Added seeders for Users, Afsrpaken and Wachtlijst

---
+++ database/seeders/WachtlijstSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Wachtlijst;
+use Faker\Factory as Faker;
+
+class WachtlijstSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $faker = Faker::create();
+        $usersWithoutAppointments = User::whereDoesntHave('afspraken')->pluck('id')->toArray();
+
+        if (empty($usersWithoutAppointments)) {
+            $this->command->info('Geen gebruikers zonder afspraken gevonden.');
+            return;
+        }
+
+        $behandelingen = ['tandheelkunde', 'orthodontie', 'endodontologie', 'parodontologie'];
+
+        foreach ($usersWithoutAppointments as $userId) {
+
+            $canMakeAppointmentAt = $faker->optional()->dateTimeBetween('now', '+2 months');
+
+            Wachtlijst::create([
+                'user_id' => $userId,
+                'added_at' => $faker->dateTimeBetween('-1 months', 'now')->format('Y-m-d'),
+                'behandeling' => $faker->randomElement($behandelingen),
+                'can_make_appointment_at' => $canMakeAppointmentAt ? $canMakeAppointmentAt->format('Y-m-d') : null,
+            ]);
+        }
+
+        $this->command->info('Wachtlijst is succesvol gevuld met gebruikers zonder afspraken.');
+    }
+}
