42c4abd74b05815f193c42e76c074090f8a55e57
lm00ns <lucasmoons@hotmail.com>
2025-01-04 21:48:12 +0100

    rijkregister nr gets hashed

---
+++ database/migrations/2025_01_04_214315_modify_rijksregister_nr_column_in_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('rijksregister_nr', 255)->change(); 
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('rijksregister_nr', 50)->change();
+        });
+    }
+};
